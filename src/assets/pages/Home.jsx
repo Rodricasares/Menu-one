1ec9bedb8ad6280f41b3dc15b41936a4ed36df85
Rodrigo <rodrigocantudo@gmail.com>
2023-11-07 12:21:14 +0100

    finishIII

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,7 +1,13 @@
 
+import './Home.css'
+import logo from '../../assets/react.svg'
 
 export function Home(){
     return(
+        <div className='home'>
         <h1>Home</h1>
+        <img src={logo} />
+      
+        </div>
     )
 }
